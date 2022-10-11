00d43237889534461b25535e2030541906a48b69
Bruno Jullien <bruno.jullien@gmail.com>
2022-10-11 16:14:51 -0700

    84. Spring Pet Clinic - Auto Generate Map IDs

--- pet-clinic-data/src/main/java/com/bbj/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/bbj/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,14 @@
 package com.bbj.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.bbj.sfgpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+//<T extends BaseEntity, ID extends Long> => helps intelliJ to help us while typing
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    //if we use ID here vs Long, Collections.max(map.keySet()) + 1; will complain
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +18,16 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("object cannot be null!");
+        }
         return object;
     }
 
@@ -29,5 +38,13 @@ public abstract class AbstractMapService<T, ID> {
     void delete(T object) {
         map.entrySet().removeIf(
                 entry -> entry.getValue().equals(object)); //will need to implement equals for our classes
+    }
+
+    private Long getNextId() {
+
+        if (map.size() == 0) {
+            return 1L;
+        }
+        return Collections.max(map.keySet()) + 1;
     }
 }

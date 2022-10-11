2a95108792c37593cd7db3ff3965e505d1ec7242
Bruno Jullien <bruno.jullien@gmail.com>
2022-10-10 19:15:48 -0700

    82. Spring Pet Clinic - List Owners

--- pet-clinic-web/src/main/java/com/bbj/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/bbj/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,27 @@
 package com.bbj.sfgpetclinic.controllers;
 
+import com.bbj.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/index", "/index.html"})
-    public String listOwners() {
-        return "owners/index";
+    public String listOwners(Model model) { //Spring MVC will inject the Model automatically when calling
+
+        model.addAttribute(
+                "owners",       //name of the property inside the model
+                ownerService.findAll()      //calling the service to return owners
+        );
+        return "owners/index";              //return the index page templates/owners/index path
     }
 }
